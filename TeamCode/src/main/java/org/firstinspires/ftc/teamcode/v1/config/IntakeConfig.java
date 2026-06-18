8dcf74e371453cb1f5a5ec8f9b4a98e32549a956
FTCTotus1Tuus <ftc19168@gmail.com>
2026-06-17 19:14:06 -0500

    enabled FTC dashboard for the intake config

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/v1/config/IntakeConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/v1/config/IntakeConfig.java
@@ -1,5 +1,8 @@
 package org.firstinspires.ftc.teamcode.v1.config;
 
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
 public class IntakeConfig {
     public static double INTAKE_POWER = 0.5;
 
